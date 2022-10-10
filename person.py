179f80b1629add4b215a510a16462e4e9229f9df
Abbos <abbosr180@gmail.com>
2022-10-10 19:09:21 +0500

    Abubakr

--- person.py
+++ person.py
@@ -29,7 +29,7 @@ video_path = detector.detectObjectsFromVideo(camera_input=camera,
     output_file_path=os.path.join(execution_path, "camera_detected_video")
     , frames_per_second=20, log_progress=True, minimum_percentage_probability=30)
 
-
+######################### Abubakr######################
 
 # car_cascade = detector.setModelPath(os.path.join(execution_path , "yolo.h5"))
 # ret, frames = camera.read()
